acd2a1b8b6283252e91598b6ef128d1ef7e935e5
Alec Cleofe <aleccleofe@gmail.com>
2022-11-09 19:14:30 -0600

    Window Setup 1
    Also added the GLFW fork as a submodule

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,11 @@ workspace "Hazel"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+IncludeDir = {}
+IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
+
+include "Hazel/vendor/GLFW"
+
 project "Hazel"
 	location "Hazel"
 	kind "SharedLib"
@@ -31,7 +36,13 @@ project "Hazel"
 	includedirs
 	{
 		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include"
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
+
+	links{
+		"GLFW",
+		"open132.lib"
 	}
 
 
